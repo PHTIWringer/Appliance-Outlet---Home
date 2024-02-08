1eb40b746b853193df58a314a9c073dde09614f7
Ken Hileman <ken.hileman@outlook.com>
2024-02-07 20:51:09 -0500

    updated photos and recliners.pc

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-"use strict";
-
 /* button scrolling*/
 
 document.querySelectorAll(".nav-link-btns button").forEach((button) => {
@@ -26,9 +24,10 @@ function displayImages() {
     index = 1;
   }
   images[index - 1].style.display = "block";
-  setTimeout(displayImages, 5000);
+  setTimeout(displayImages, 10000);
 }
 
+let index1 = 0;
 displayImages1();
 function displayImages1() {
   let i;
@@ -36,25 +35,42 @@ function displayImages1() {
   for (i = 0; i < images1.length; i++) {
     images1[i].style.display = "none";
   }
-  index++;
-  if (index > images1.length) {
-    index = 1;
+  index1++;
+  if (index1 > images1.length) {
+    index1 = 1;
   }
-  images1[index - 1].style.display = "block";
-  setTimeout(displayImages1, 5000);
+  images1[index1 - 1].style.display = "block";
+  setTimeout(displayImages1, 13000);
 }
 
+let index2 = 0;
 displayImages2();
 function displayImages2() {
   let i;
-  const images2 = document.getElementsByClassName("image-fade-rugs");
+  const images2 = document.getElementsByClassName("image-fade-recliner");
   for (i = 0; i < images2.length; i++) {
     images2[i].style.display = "none";
   }
-  index++;
-  if (index > images2.length) {
-    index = 1;
+  index2++;
+  if (index2 > images2.length) {
+    index2 = 1;
   }
-  images2[index - 1].style.display = "block";
-  setTimeout(displayImages2, 5000);
+  images2[index2 - 1].style.display = "block";
+  setTimeout(displayImages2, 15000);
+}
+
+let index3 = 0;
+displayImages3();
+function displayImages3() {
+  let i;
+  const images3 = document.getElementsByClassName("image-fade-rugs");
+  for (i = 0; i < images3.length; i++) {
+    images3[i].style.display = "none";
+  }
+  index3++;
+  if (index3 > images3.length) {
+    index3 = 1;
+  }
+  images3[index3 - 1].style.display = "block";
+  setTimeout(displayImages3, 17000);
 }
