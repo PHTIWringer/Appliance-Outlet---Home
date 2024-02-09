e777c42b1427102b324a3a3837ab6d493fb55863
Ken Hileman <ken.hileman@outlook.com>
2024-02-08 19:54:36 -0500

    Refactored JS.pc

--- script.js
+++ script.js
@@ -11,69 +11,31 @@ document.querySelectorAll(".nav-link-btns button").forEach((button) => {
 
 /* IMG slideshows */
 
-let index = 0;
-displayImages();
-function displayImages() {
-  let i;
-  const images = document.getElementsByClassName("image-fade");
-  for (i = 0; i < images.length; i++) {
-    images[i].style.display = "none";
+function startSlideshow(className, duration) {
+  let index = 0; // Initialize index for each slideshow
+
+  function displayImages() {
+    const images = document.getElementsByClassName(className);
+    for (let i = 0; i < images.length; i++) {
+      images[i].style.display = "none"; // Hide all images
+    }
+    index++;
+    if (index > images.length) {
+      index = 1; // Reset index if it exceeds the number of images
+    }
+    images[index - 1].style.display = "block"; // Show the current image
+
+    setTimeout(displayImages, duration); // Set the timeout for the next image
   }
-  index++;
-  if (index > images.length) {
-    index = 1;
-  }
-  images[index - 1].style.display = "block";
-  setTimeout(displayImages, 10000);
+
+  displayImages(); // Start the slideshow
 }
 
-let index1 = 0;
-displayImages1();
-function displayImages1() {
-  let i;
-  const images1 = document.getElementsByClassName("image-fade-loveseat");
-  for (i = 0; i < images1.length; i++) {
-    images1[i].style.display = "none";
-  }
-  index1++;
-  if (index1 > images1.length) {
-    index1 = 1;
-  }
-  images1[index1 - 1].style.display = "block";
-  setTimeout(displayImages1, 13000);
-}
-
-let index2 = 0;
-displayImages2();
-function displayImages2() {
-  let i;
-  const images2 = document.getElementsByClassName("image-fade-recliner");
-  for (i = 0; i < images2.length; i++) {
-    images2[i].style.display = "none";
-  }
-  index2++;
-  if (index2 > images2.length) {
-    index2 = 1;
-  }
-  images2[index2 - 1].style.display = "block";
-  setTimeout(displayImages2, 15000);
-}
-
-let index3 = 0;
-displayImages3();
-function displayImages3() {
-  let i;
-  const images3 = document.getElementsByClassName("image-fade-rugs");
-  for (i = 0; i < images3.length; i++) {
-    images3[i].style.display = "none";
-  }
-  index3++;
-  if (index3 > images3.length) {
-    index3 = 1;
-  }
-  images3[index3 - 1].style.display = "block";
-  setTimeout(displayImages3, 17000);
-}
+// Initialize all slideshows with different classes and durations
+startSlideshow("image-fade", 10000); // 10 seconds
+startSlideshow("image-fade-loveseat", 13000); // 13 seconds
+startSlideshow("image-fade-recliner", 15000); // 15 seconds
+startSlideshow("image-fade-rugs", 17000); // 17 seconds
 
 /* Scroll to Top */
 
